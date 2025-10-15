95d020a376063b5c7862706e4bcd458797013a22
kiendt51 <kiendt51@fpt.com>
2025-10-16 06:49:55 +0700

    handle conflict when merge branch namkuner into main

--- apps/teachers/serializers/class.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apps.my_built_in.models.class_ import Class
-
-class ClassDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Class  # Replace with your Class model
-        fields = ['id', 'name', 'major', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at']
--- apps/teachers/serializers/course_session.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from apps.my_built_in.models.course_session import CourseSession
-
-
-class CourseSessionDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CourseSession
-        fields = '__all__'
