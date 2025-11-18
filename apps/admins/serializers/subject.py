a3046d084f04cfd95771f59ff52eeb4fd6a74abd
kiendt51 <dr.kientrdoan@gmail.com>
2025-11-19 00:49:08 +0700

    update

--- apps/admins/serializers/subject.py
+++ apps/admins/serializers/subject.py
@@ -19,15 +19,23 @@ class SubjectListSerializer(serializers.ModelSerializer):
         return None
 
 class SubjectSerializer(serializers.ModelSerializer):
+    department = serializers.SerializerMethodField()
     class Meta:
         model = MonHoc
-        fields = ['id', 'code', 'name', 'credit', 'description', 'total_period', 'major', 'is_deleted', 'created_at', 'updated_at']
+        fields = ['id', 'code', 'name', 'credit', 'description', 'total_period', 'major', 'department', 'is_deleted', 'created_at', 'updated_at']
         read_only_fields = ['id']
+
+    def get_department(self, obj):
+        department = obj.major.department
+        if department:
+            return department.id
+        return None
 
 class SubjectCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = MonHoc
         fields = ['code', 'name', 'credit', 'description', 'total_period', 'major']
+    
 
 class SubjectUpdateSerializer(serializers.ModelSerializer):
     class Meta:
